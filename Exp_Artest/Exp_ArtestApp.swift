6185dd4d66e0a741628643deff4c002225b272db
kwrsin <kwrsin@gmail.com>
2020-11-23 19:49:25 +0900

    Initial Commit

---
+++ Exp_Artest/Exp_ArtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exp_ArtestApp.swift
+//  Exp_Artest
+//
+//  Created by kwrsin on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Exp_ArtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
