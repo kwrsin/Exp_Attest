3ae8cc5da36da4fe62cbed41dc73c3ba70fb429c
kwrsin <kwrsin@gmail.com>
2020-12-22 08:09:17 +0900

    challenge and keyId generation

--- Exp_Attest/ContentView.swift
+++ Exp_Attest/ContentView.swift
@@ -17,9 +17,12 @@ struct ContentView: View {
     @State var removed = ""
     static let domain = "http://192.168.11.2:4567"
     var body: some View {
-        Text("status: \(attestResponse)")
         Text(challenge)
             .padding()
+        Button(action: {
+            getChallenge()
+        }, label: {Text("challenge")})
+        Text("status: \(attestResponse)")
         Button(action: {
             attest()
         }, label: {Text("Attest")})
@@ -145,8 +148,8 @@ struct ContentView: View {
 
         }
     }
-
-    func attest() {
+    
+    func getChallenge() {
         let service = DCAppAttestService.shared
         if service.isSupported {
             let req_challenge = URLRequest(url: URL(string: "\(Self.domain)/challenge")!)
@@ -161,76 +164,83 @@ struct ContentView: View {
                     var result = [String:Any]()
                     do {
                         result = try JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
+                        let root = result["challenge"] as! [String: Any]
+                        let uuid = root["uuid"] as! String
+                        challenge = uuid
                     } catch let err {
                         print(err)
                     }
-                    DCAppAttestService.shared.generateKey() { keyId, error in
-                        guard error == nil else {
-                            if let error = error {
-                                print(error.localizedDescription)
-                            }
-                            return
-                        }
+                }.resume()
+            }
+        }
+        
+
+    }
+
+    func attest() {
+        if challenge.isEmpty == false {
+            DCAppAttestService.shared.generateKey() { keyId, error in
+                guard error == nil else {
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    return
+                }
+                
+                let uuid = challenge
+                let hashedChallenge = Data(SHA256.hash(data: uuid.data(using: .utf8)!))
+                DCAppAttestService.shared.attestKey(keyId!, clientDataHash: hashedChallenge) { attestation, error in
+                    if let error = error {
+                        //FIXME: if gettting a serverUnavailable error retry it again. if you got aother error, recreate key again.
+                        print(error.localizedDescription)
+                        return
+                    }
+                    
+                    if let attestation = attestation, let keyId = keyId {
+                        var req_attestation = URLRequest(url: URL(string: "\(Self.domain)/attestation/\(uuid)")!)
                         
-                        let root = result["challenge"] as! [String: Any]
-                        let uuid = root["uuid"] as! String
-                        let hashedChallenge = Data(SHA256.hash(data: uuid.data(using: .utf8)!))
-                        DCAppAttestService.shared.attestKey(keyId!, clientDataHash: hashedChallenge) { attestation, error in
-                            if let error = error {
-                                //FIXME: if gettting a serverUnavailable error retry it again. if you got aother error, recreate key again.
-                                print(error.localizedDescription)
+                        req_attestation.httpMethod = "POST"
+
+                        var attestationObject = [String: Any]()
+                        attestationObject["attestation"] = attestation.base64EncodedString(options: [])
+                            .addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
+                        attestationObject["keyId"] = keyId
+                            .addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
+                        var params = [String]()
+                        attestationObject.forEach { key, value in
+                            params.append("\(key)=\(value)")
+                        }
+                        req_attestation.httpBody = params.joined(separator: "&").data(using: .utf8)
+                        req_attestation.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+                        
+                        URLSession.shared.dataTask(with: req_attestation) {data, res, error in
+                            guard let data = data, let res = res else {
+                                if let error = error {
+                                    print(error.localizedDescription)
+                                }
+                                
                                 return
                             }
-                            
-                            if let attestation = attestation, let keyId = keyId {
-                                var req_attestation = URLRequest(url: URL(string: "\(Self.domain)/attestation/\(uuid)")!)
-                                
-                                req_attestation.httpMethod = "POST"
-
-                                var attestationObject = [String: Any]()
-                                attestationObject["attestation"] = attestation.base64EncodedString(options: [])
-                                    .addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
-                                attestationObject["keyId"] = keyId
-                                    .addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
-                                var params = [String]()
-                                attestationObject.forEach { key, value in
-                                    params.append("\(key)=\(value)")
-                                }
-                                req_attestation.httpBody = params.joined(separator: "&").data(using: .utf8)
-                                req_attestation.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-                                
-                                URLSession.shared.dataTask(with: req_attestation) {data, res, error in
-                                    guard let data = data, let res = res else {
-                                        if let error = error {
-                                            print(error.localizedDescription)
-                                        }
-                                        
-                                        return
-                                    }
-                                    let httpres = res as! HTTPURLResponse
-                                    print(httpres.statusCode)
-                                    //TODO: save the keyId, if getting a key/pair's keyId successfully.
-                                    UserDefaults.standard.setValue(keyId, forKey: "attest_key")
-                                    UserDefaults.standard.setValue(uuid, forKey: "uuid")
-                                    var result = [String:Any]()
-                                    do {
-                                        result = try JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
-                                    } catch let err {
-                                        print(err)
-                                    }
-                                    let status = result["result"] as! Int
-                                    DispatchQueue.main.async {
-                                        attestResponse = status
-                                    }
-                                }.resume()
-
+                            let httpres = res as! HTTPURLResponse
+                            print(httpres.statusCode)
+                            //TODO: save the keyId, if getting a key/pair's keyId successfully.
+                            UserDefaults.standard.setValue(keyId, forKey: "attest_key")
+                            UserDefaults.standard.setValue(uuid, forKey: "uuid")
+                            var result = [String:Any]()
+                            do {
+                                result = try JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
+                            } catch let err {
+                                print(err)
                             }
-                        }
-                        
-                    }
+                            let status = result["result"] as! Int
+                            DispatchQueue.main.async {
+                                attestResponse = status
+                            }
+                        }.resume()
 
-                    
-                }.resume()
+                    }
+                }
+                
             }
         }
         
